7b6a50482ffe6abd1d94c847568bcd95cad59a4c
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2021-07-23 20:55:30 -0500

    dockerfile

--- run_test.sh
+++ run_test.sh
@@ -2,7 +2,7 @@
 #
 # Example docker command:
 #
-# docker run -it -v $(pwd)/INPUTS:/INPUTS <image_id> /INPUTS/test.html /INPUTS
+# docker run -v $(pwd)/INPUTS:/INPUTS <image_id> /INPUTS/test.html /INPUTS
 
 html_file=$1
 out_dir=$2
